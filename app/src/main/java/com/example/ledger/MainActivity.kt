9dfdcf095291c74b12ac85c26d4e62f50a80aeb8
ahaocoder <ahao.coder@gmail.com>
2024-01-07 20:19:08 +0800

    3

--- app/src/main/java/com/example/ledger/MainActivity.kt
+++ app/src/main/java/com/example/ledger/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.ledger
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -11,6 +13,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -40,39 +43,63 @@ class MainActivity : ComponentActivity() {
 fun MainPage(){
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = "login") {
-        composable("login") { LoginPage(navController) }
+    NavHost(navController = navController, startDestination = "home") {
         composable("home") { HomePage(navController) }
+        composable("setting") { SettingPage(navController) }
+        composable("profile") { ProfilePage(navController) }
     }
+
 
 }
 
-@Composable
-fun LoginPage(navController: NavController) {
-    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxSize()) {
-
-        Text(text = "登录页", Modifier.padding(top = 80.dp))
-
-        Button(modifier = Modifier.padding(top = 80.dp), onClick = {
-            navController.navigate("home")
-        }) {
-            Text(text = "跳转到home")
-
-        }
-
-    }
-}
 @Composable
 fun HomePage(navController: NavController) {
     Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxSize()) {
-        Text(text = "主页", Modifier.padding(top = 80.dp))
+
+        Text(text = "记账主页", Modifier.padding(top = 80.dp))
+
         Button(modifier = Modifier.padding(top = 80.dp), onClick = {
-            navController.popBackStack()
+            navController.navigate("setting")
         }) {
-            Text(text = "返回")
+            Text(text = "跳转到设置")
+
+        }
+
+        Button(modifier = Modifier.padding(top = 80.dp), onClick = {
+            navController.navigate("profile")
+        }) {
+            Text(text = "跳转到详情")
+
+        }
+    }
+
+}
+
+@Composable
+fun ProfilePage(navController: NavController){
+    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxSize()) {
+        Text(text = "数据页", Modifier.padding(top = 80.dp))
+        Button(modifier = Modifier.padding(top = 80.dp), onClick = {
+            navController.navigate("home")
+        }) {
+            Text(text = "返回主页")
         }
     }
 }
+
+@Composable
+fun SettingPage(navController: NavController) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxSize()) {
+        Text(text = "设置页", Modifier.padding(top = 80.dp))
+        Button(modifier = Modifier.padding(top = 80.dp), onClick = {
+            navController.navigate("home")
+        }) {
+            Text(text = "返回主页")
+        }
+    }
+}
+
+
 
 @Preview(showBackground = true)
 @Composable
